b53ee933763a7511a0d8ab3ced3fa3a15eaa336c
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-08-21 18:31:03 +0000

    avance

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,10 +1,11 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
 
 
 api = Blueprint('api', __name__)
@@ -19,6 +20,13 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-@api.route('/signup', methods=['POST'])
+@api.route('/signup', methods=['POST', 'GET'])
 def handle_register():
-    request_body = jsonify()
+    request_body = request.get_json()
+    new_user = User()
+    new_user.email=request_body["email"]
+    new_user.password=request_body["password"]
+    new_user.is_active=request_body["is_active"]
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message":"texto"})
